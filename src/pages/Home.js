389ec024a96dd6aa597a8f70ca1667d73d2e16cb
jeedeekay <gavinkinder@gmail.com>
2023-05-14 19:07:43 -0700

    Init

---
+++ src/pages/Home.js
@@ -0,0 +1,17 @@
+import { 
+    Container,
+    Row,
+    Col
+} from 'reactstrap';
+
+const Home = () => {
+    return (
+        <Container>
+            <Row>
+                <Col></Col>
+            </Row>
+        </Container>
+    );
+}
+
+export default Home;
